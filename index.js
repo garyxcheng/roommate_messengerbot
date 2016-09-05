708cb69f1573893b3fd3215f0f140345cf9cbabc
Gary Cheng <garycheng@berkeley.edu>
2016-09-04 23:20:51 -0700

    added postgress to my database

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (err) throw err;
+    console.log('Connected to postgres! Getting schemas...');
+
+    client
+        .query('SELECT table_schema,table_name FROM information_schema.tables;')
+        .on('row', function(row) {
+	    console.log(JSON.stringify(row));
+	});
+});
 
 app.set('port', (process.env.PORT || 5000))
 
