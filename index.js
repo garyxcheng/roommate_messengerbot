06087699e506a9ec66dece74b8704aee1c8d234b
Gary Cheng <garycheng@berkeley.edu>
2016-09-04 23:56:10 -0700

    made the client stuff maube

--- index.js
+++ index.js
@@ -6,17 +6,9 @@ const request = require('request')
 const app = express()
 var pg = require('pg');
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-    if (err) throw err;
-    console.log('Connected to postgres! Getting schemas...');
-
-    client
-        .query('SELECT table_schema,table_name FROM information_schema.tables;')
-        .on('row', function(row) {
-	    console.log(JSON.stringify(row));
-	});
-});
+var connectionString = process.env.DATABASE_URL;
+var client = new pg.Client(connectionString);
+client.connect();
 
 app.set('port', (process.env.PORT || 5000))
 
