32830213d59586d708dd79e1816823d69aea643e
Gary Cheng <garycheng@berkeley.edu>
2016-09-06 21:29:54 -0700

    added pg.defaults

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const request = require('request')
 const app = express()
 var pg = require('pg');
 
+pg.defaults.ssl = true;
+
 var connectionString = "postgres://sciefmdltovpgf:_ZvjMSKxSioNzHzthDMMlQ5Ziw@ec2-54-235-68-4.compute-1.amazonaws.com:5432/deut9i6kcu526n";
 var client = new pg.Client(connectionString);
 client.connect();
